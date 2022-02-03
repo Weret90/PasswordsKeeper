e75605f4dfd680b5004729c7e160e3d1f523f5e5
Ермолаев Архип Федотович <arkhip.ermolaev@mail.ru>
2022-02-03 15:06:32 +0900

    Добавил простой тест для фрагмента (отличий от тестирования активити почти нет, потому не стал сильно напирать с тестами)

--- app/src/main/java/com/umbrella/passwordskeeper/presentation/MainActivity.kt
+++ app/src/main/java/com/umbrella/passwordskeeper/presentation/MainActivity.kt
@@ -3,10 +3,15 @@ package com.umbrella.passwordskeeper.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.umbrella.passwordskeeper.R
+import com.umbrella.passwordskeeper.presentation.fragments.AuthFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, AuthFragment())
+            .commit()
     }
 }
